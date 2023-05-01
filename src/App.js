0a3857196bcb64b5447e2de834bc6c20629d91f3
Dwight Lindquist <dwight.lindquist@gmail.com>
2023-05-01 10:26:01 -0400

    can create new cart at registration, put cart in state, and put current username in navbar

--- src/App.js
+++ src/App.js
@@ -4,17 +4,27 @@ import NavbarComponent from "./components/NavbarComponent";
 import Register from "./pages/Register"
 import Home from "./pages/Home";
 import { Nav } from "react-bootstrap";
+import Login from "./pages/Login";
 
 
 function App() {
+
+  const [currentUsername, setCurrentUsername] = useState();
+  // email is unique in db
+  const [currentEmail, setCurrentUserEmail] = useState();
+  const [currentUserId, setCurrentUserId] = useState();
+  const [currentToken, setCurrentToken] = useState();
+  const [cart, setCart] = useState();
+
   return (
     <div className="App">
       <BrowserRouter>
-      <NavbarComponent />
+      <NavbarComponent currentUsername={currentUsername} setCurrentUsername={setCurrentUsername} />
         <div>
           <Routes>
             <Route path="/" element={<Home />} />
-            <Route path="/register" element={<Register />} />
+            <Route path="/register" element={<Register setCart={setCart} setCurrentToken={setCurrentToken} setCurrentUsername={setCurrentUsername} setCurrentUserEmail={setCurrentUserEmail} setCurrentUserId={setCurrentUserId} />} />
+            <Route path="/login" element={<Login setCart={setCart} setCurrentToken={setCurrentToken} setCurrentUsername={setCurrentUsername} setCurrentUserEmail={setCurrentUserEmail} setCurrentUserId={setCurrentUserId} />} />
           </Routes>
         </div>
       </BrowserRouter>
