b45be91046740da4802d17999d622d0eabc3cfdf
Dwight Lindquist <dwight.lindquist@gmail.com>
2023-05-15 10:38:52 -0400

    logging getCartUrl on checkout success

--- src/pages/Success.js
+++ src/pages/Success.js
@@ -9,37 +9,38 @@ function Success({ cart, setCart, currentToken, currentUserId, setItemsProductsD
     let baseURL = "https://store2-backend.herokuapp.com"
     let getCartUrl = baseURL + "/api/carts/" + currentUserId 
 
-    let reqHeaders = {
-      headers:{
-        "Accept": "application/json",
-        Authorization: `Bearer ${currentToken}`
-      }
-    }
+    console.log("get new cart url: ", getCartUrl)
+    // let reqHeaders = {
+    //   headers:{
+    //     "Accept": "application/json",
+    //     Authorization: `Bearer ${currentToken}`
+    //   }
+    // }
 
-    try {
-      const res = await axios.get(getCartUrl, reqHeaders)
+    // try {
+    //   const res = await axios.get(getCartUrl, reqHeaders)
 
-      if (res.data[0] !== cart){
-        // set state 
-        setCart(res.data[0])
-        setItemsProductsData(null);
-      }
+    //   if (res.data[0] !== cart){
+    //     // set state 
+    //     setCart(res.data[0])
+    //     setItemsProductsData(null);
+    //   }
 
-    } catch (err) {
-      if (err.response.data.message === "Unauthenticated."){
-        localStorage.removeItem("store2-user")
-      } else {
-        console.log("error from Success.js useEffect: ", err)
-      }
-    } 
+    // } catch (err) {
+    //   if (err.response.data.message === "Unauthenticated."){
+    //     localStorage.removeItem("store2-user")
+    //   } else {
+    //     console.log("error from Success.js useEffect: ", err)
+    //   }
+    // } 
 
   }
 
-  // runs on every render 
+  // runs on first render 
   useEffect(() => {
     console.log("Success.js useEffect called");
     getNewCart();
-    });
+    }, []);
 
   return (
     <div>The cart checkout session succeeded.</div>
