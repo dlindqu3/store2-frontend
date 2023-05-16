f919e925f098fc60151695faeb198dd6b986112b
Dwight Lindquist <dwight.lindquist@gmail.com>
2023-05-16 11:18:54 -0400

    updated orders page

--- src/pages/Orders.js
+++ src/pages/Orders.js
@@ -4,44 +4,50 @@ import axios from 'axios';
 function Orders({ currentToken, currentUserId }) {
 
   const [isLoading, setIsLoading] = useState(true);
+  const [noOrdersText, setNoOrdersText] = useState(true);
   const [orders, setOrders] = useState();
 
   let baseURL = "https://store2-backend.herokuapp.com"
-  
+
+  let reqHeaders = {
+    headers: {
+      Accept: "application/json",
+      Authorization: `Bearer ${currentToken}`,
+    },
+  };
 
   let getOrders = async () => {
     let reqUrl = baseURL + "/api/orders_for_user/" + currentUserId
-    let reqHeaders = {
-      headers: {
-        Accept: "application/json",
-        Authorization: `Bearer ${currentToken}`,
-      },
-    };
+
     let res = await axios.get(reqUrl, reqHeaders)
     console.log("res from get orders for user: ", res)
     // res.data is an array of order objects 
     setOrders(res.data)
+
+    if (res.data.length > 0){ 
+      setNoOrdersText(false); 
+    }
   }
 
   // runs only on first render
   useEffect(() => {
     console.log("Order.js useEffect called")
-    console.log("currentUserId: " + currentUserId)
-    console.log("currentToken: " + currentToken)
-
     // THIS CAUSES A 401 ERROR - UNAUTHORIZED
     // THIS ALSO CAUSES A MESSAGE: UNAUTHENTICAED.
     getOrders()
     setIsLoading(false); 
-  }, [currentToken, currentUserId]);
+  }, []);
 
   return (
     <div>
       <div>Orders page here</div>
-      { isLoading && <p>Loading...</p> }
+      { noOrdersText && <p>You have not made any orders.</p> }
+      { isLoading ? <p>Loading...</p>: <p></p> }
+
+      { orders && console.log("orders state: ", orders) }
       { orders && orders.map((obj) =>{
-        <p>{JSON.stringify(obj)}</p>
-      })}
+        <p key={obj.id}>{obj["total_cost"]}</p>
+      }) }
     </div>
   )
 }
