83b224604f1b998a2bd25c9f259f351c2057ee12
Dwight Lindquist <dwight.lindquist@gmail.com>
2023-05-18 08:48:36 -0400

    can display orders data as a table

--- src/pages/Orders.js
+++ src/pages/Orders.js
@@ -1,149 +1,163 @@
-import React, { useState, useEffect } from 'react'
-import axios from 'axios';
+import React, { useState, useEffect } from "react";
+import Table from "react-bootstrap/Table";
+import Button from "react-bootstrap/Button";
+import axios from "axios";
 
 function Orders({ currentToken, currentUserId }) {
-
   const [isLoading, setIsLoading] = useState(true);
   const [noOrdersText, setNoOrdersText] = useState(true);
   const [ordersData, setOrdersData] = useState();
 
-  let baseURL = "https://store2-backend.herokuapp.com"
+  let baseURL = "https://store2-backend.herokuapp.com";
 
   let reqHeaders = {
-    headers:{
-      "Accept": "application/json",
-      Authorization: `Bearer ${currentToken}`
-    }
-  }
+    headers: {
+      Accept: "application/json",
+      Authorization: `Bearer ${currentToken}`,
+    },
+  };
 
-
-  let getOrdersAndItems = async () => {    
-
-    // WORKS 
-    // 1. get all orders for user 
-    let reqUrl1 = baseURL + "/api/orders_for_user/" + currentUserId
-    let res = await axios.get(reqUrl1, reqHeaders)
-    console.log("orders for user: ", res.data)
+  let getOrdersAndItems = async () => {
+    // WORKS
+    // 1. get all orders for user
+    let reqUrl1 = baseURL + "/api/orders_for_user/" + currentUserId;
+    let res = await axios.get(reqUrl1, reqHeaders);
+    // console.log("orders for user: ", res.data);
     let orders = res.data;
-    
-    // DOESN'T WORK 
-    // 2. get all order_items for user 
+
+    // DOESN'T WORK
+    // 2. get all order_items for user
     let orderItems = [];
-    for (let i = 0; i < orders.length; i++){
+    for (let i = 0; i < orders.length; i++) {
       let currentOrder = orders[i];
-      let reqUrl2 = baseURL + "/api/order_items_for_order/" + currentOrder.id 
-      let res2 = await axios.get(reqUrl2, reqHeaders)
-      console.log("res2 order items for order: ", res2.data)
-      for (let j = 0; j < res2.data.length; j++){
-        orderItems.push(res2.data[j])
+      let reqUrl2 = baseURL + "/api/order_items_for_order/" + currentOrder.id;
+      let res2 = await axios.get(reqUrl2, reqHeaders);
+      // console.log("res2 order items for order: ", res2.data);
+      for (let j = 0; j < res2.data.length; j++) {
+        orderItems.push(res2.data[j]);
       }
     }
-    console.log("all orderItems for user: ", orderItems)
+    // console.log("all orderItems for user: ", orderItems);
 
     // NEW
     // 3. get all unique product_ids for user
     let uniqueProductIds = [];
-    for (let j = 0; j < orderItems.length; j++){
+    for (let j = 0; j < orderItems.length; j++) {
       let currentOrderItem = orderItems[j];
-      if (!uniqueProductIds.includes(currentOrderItem.product_id)){
-        uniqueProductIds.push(currentOrderItem.product_id)
+      if (!uniqueProductIds.includes(currentOrderItem.product_id)) {
+        uniqueProductIds.push(currentOrderItem.product_id);
       }
     }
-    console.log("unique products bought: ", uniqueProductIds)
+    // console.log("unique products bought: ", uniqueProductIds);
 
-    // // 4. get product items with uniqueProductIds array  
-    let reqUrl3 = baseURL + "/api/products/filter"
-    let reqBody3 = { "productIds": uniqueProductIds }
-    let res3 = await axios.post(reqUrl3, reqBody3, reqHeaders)
-    let filteredProducts = res3.data
-    console.log("filtered products: ", filteredProducts)
+    // // 4. get product items with uniqueProductIds array
+    let reqUrl3 = baseURL + "/api/products/filter";
+    let reqBody3 = { productIds: uniqueProductIds };
+    let res3 = await axios.post(reqUrl3, reqBody3, reqHeaders);
+    let filteredProducts = res3.data;
+    // console.log("filtered products: ", filteredProducts);
 
-    // // 5. start empty ARRAY 
+    // // 5. start empty ARRAY
     let resArray = [];
 
-    // // 6. add OBJECT for each order to ARRAY 
-    for (let i = 0; i < orders.length; i++){
+    // // 6. add OBJECT for each order to ARRAY
+    for (let i = 0; i < orders.length; i++) {
       resArray.push({
-        "orderId": orders[i]["id"],
-        "time": orders[i]["created_at"],
-        "totalCost": orders[i]["total_cost"],
-        "productsAndQuants": []
-      })
+        orderId: orders[i]["id"],
+        time: orders[i]["created_at"],
+        totalCost: orders[i]["total_cost"],
+        productsAndQuants: [],
+      });
     }
 
-    // // 7. add OBJ to productsAndQuants for each product/orderItem with name, quantity 
-    for (let i = 0; i < resArray.length; i++){
+    // // 7. add OBJ to productsAndQuants for each product/orderItem with name, quantity
+    for (let i = 0; i < resArray.length; i++) {
       let currentOrder = resArray[i];
-      let currentOrderId = currentOrder.orderId
+      let currentOrderId = currentOrder.orderId;
 
-      let filteredItems = []
-      for (let k = 0; k < orderItems.length; k++){
-        if (orderItems[k].order_id === currentOrderId){
-          filteredItems.push(orderItems[k])
+      let filteredItems = [];
+      for (let k = 0; k < orderItems.length; k++) {
+        if (orderItems[k].order_id === currentOrderId) {
+          filteredItems.push(orderItems[k]);
         }
       }
 
-      for (let j = 0; j < filteredItems.length; j++){
+      for (let j = 0; j < filteredItems.length; j++) {
         let currentItem = filteredItems[j];
-        console.log("current filtered order item: ", currentItem)
-        let productQuant = {}
-        let currentItemProdId = currentItem.product_id
-        for (let k = 0; k < filteredProducts.length; k++){
+        // console.log("current filtered order item: ", currentItem);
+        let productQuant = {};
+        let currentItemProdId = currentItem.product_id;
+        for (let k = 0; k < filteredProducts.length; k++) {
           let currentProduct = filteredProducts[k];
-          if (currentItemProdId === currentProduct.id){
-            productQuant["name"] = currentProduct["name"]
+          if (currentItemProdId === currentProduct.id) {
+            productQuant["name"] = currentProduct["name"];
             productQuant["quantity"] = currentItem["quantity"];
-            console.log("finished productQuant item: ", productQuant)
-          currentOrder["productsAndQuants"].push(productQuant)
+            // console.log("finished productQuant item: ", productQuant);
+            currentOrder["productsAndQuants"].push(productQuant);
           }
         }
       }
     }
 
-    console.log("resArray from getOrdersAndItems: ", resArray)
+    // console.log("resArray from getOrdersAndItems: ", resArray);
 
-    // 8. setState with ARRAY  
-    setOrdersData(resArray)
-  }
+    // 8. setState with ARRAY
+    setOrdersData(resArray);
+  };
 
   // runs only on first render
   useEffect(() => {
-    console.log("Order.js useEffect called")
-    getOrdersAndItems()
-    setIsLoading(false); 
+    console.log("Order.js useEffect called");
+    getOrdersAndItems();
+    setIsLoading(false);
   }, []);
 
-  // display order date, total cost, each product name, and quantity for each product 
+  // display order date, total cost, each product name, and quantity for each product
 
   return (
     <div>
       <div>Orders page here</div>
-      { noOrdersText && <p>You have not made any orders.</p> }
-      { isLoading ? <p>Loading...</p>: <p></p> }
+      { !ordersData.length > 0 && <p>You have not made any orders.</p> }
+      {isLoading ? <p>Loading...</p> : <p></p>}
 
-      {/* { ordersAndItems && console.log("ordersAndItems state: ", ordersAndItems) }
-      { ordersAndItems && <p>{JSON.stringify(ordersAndItems)} </p> } */}
+      {ordersData && ordersData.length > 0 && console.log("ordersData: ", ordersData)}
 
-      {/* <br /> 
-      <br />  */}
-
-      { ordersData && Object.keys(ordersData).map((keyData) => {
-        return  <div key={keyData}>
-                  <p>{ordersData[keyData]["time"]}</p>
-                  {/* <p>{ordersData[keyData]["totalCost"]}</p> */}
-                  {
-                    ordersData[keyData]["productsAndQuants"].map((item) => {
-                      return <div key={item.name}>
-                              <p>{item.name} x{item.quantity}</p>
-                            </div>
-                    })
-                  }
-                  <p>Total cost: {ordersData[keyData]["totalCost"]}</p>
-                </div>
-      }) }
+      {ordersData && ordersData.length > 0 && (
+        <Table striped bordered hover>
+          <thead>
+            <tr>
+              <th>#</th>
+              <th>Date</th>
+              <th>Products</th>
+              <th>Total Cost</th>
+            </tr>
+          </thead>
+          <tbody>
+            {ordersData &&
+              ordersData.map((order, index) => {
+                return (
+                  <tr key={index}>
+                    <td>{ordersData.indexOf(order) + 1}</td>
+                    <td>{order.time}</td>
+                    <td>
+                      {order.productsAndQuants.map((product) => {
+                        return (
+                          <p key={product.name}>
+                            {product.name} x {product.quantity}
+                          </p>
+                        );
+                      })}
+                    </td>
+                    <td>${order.totalCost}.00</td>
+                  </tr>
+                );
+              })}
+          </tbody>
+        </Table>
+      )}
 
     </div>
-  )
+  );
 }
 
-export default Orders
+export default Orders;
